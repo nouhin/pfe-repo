fee8df7232b52c54bd628c51221401965d900d07
nouhin <nohaylaajmi@gmail.com>
2021-09-09 08:44:41 +0200

    Add google colab nnotebooks

--- analyse.py
+++ analyse.py
@@ -3,16 +3,16 @@ from tkinter import filedialog, Tk
 
 root = Tk()
 root.withdraw()
-TOMO_LABELS_DIR = filedialog.askdirectory( title = "Choose Destination Directory for Labels...")
+TOMO_LABELS_DIR = filedialog.askdirectory( title = "Choose Destination Directory for Modified Labels...")
 file = filedialog.askopenfilename(title="Choose a .dat file")
 
 root.destroy()
 
 df = load_tomo_data(file)
 df = clean_tomo_data(df)
-df.to_csv(TOMO_LABELS_DIR +"/"+file)
+df.to_csv(TOMO_LABELS_DIR +"/output.xlsx")
 
 new_df = compute_indicators_porosity_level(df, 8691600.00)
-new_df.to_csv(TOMO_LABELS_DIR +"/"+file)
+new_df.to_csv(TOMO_LABELS_DIR +"/output2.xlsx")
 
 
